ab06024e851fdc5e2bad3b6f5452266cfd208f11
okajima shion <93690575+Otankonas@users.noreply.github.com>
2022-08-20 07:06:11 +0900

    TODO Completed!!!!

--- src/index.js
+++ src/index.js
@@ -5,19 +5,59 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// delete from list
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// create incomplete-list
+const createIncompleteList = (text) => {
   // make div
   const div = document.createElement("div");
   div.className = "list-row";
 
   // make list
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // make button
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    const completeTarget = completeButton.parentNode;
+    deleteFromIncompleteList(completeTarget);
+
+    // add complete-list
+    // get TODO-text
+    const text = completeTarget.firstElementChild.innerText;
+
+    //init
+    completeTarget.textContent = null;
+
+    //make li tag
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //make return button
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    //set div tag child
+    completeTarget.appendChild(li);
+    completeTarget.appendChild(backButton);
+
+    // set complete-list append child
+    document.getElementById("complete-list").appendChild(completeTarget);
   });
 
   const deleteButton = document.createElement("button");
@@ -25,7 +65,7 @@ const onClickAdd = () => {
   deleteButton.addEventListener("click", () => {
     // delete pushed button tag(div)
     const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteTarget);
   });
 
   // set div child element
